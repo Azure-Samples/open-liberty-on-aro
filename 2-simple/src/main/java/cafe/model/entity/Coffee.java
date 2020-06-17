acc87fba4cdd3a74415ef06577c9ad194692c569
Jianguo Ma <jiangma@microsoft.com>
2020-06-18 06:20:46 +0800

    move samples used in doc to Azure-Samples/open-liberty-on-aro

---
+++ 2-simple/src/main/java/cafe/model/entity/Coffee.java
@@ -0,0 +1,71 @@
+package cafe.model.entity;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Coffee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	protected String name;
+	protected Double price;
+
+	public Coffee() {
+	}
+
+	public Coffee(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (id != null ? id.hashCode() : 0);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (!(object instanceof Coffee)) {
+			return false;
+		}
+		Coffee other = (Coffee) object;
+		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "cafe.model.entity.Coffee[id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+}
