704143926ad8f37121ab99a631569d27b8cb48b6
Jianguo Ma <jiangma@microsoft.com>
2020-10-10 17:33:10 +0800

    change JSF view and its backed bean to be stateless for database integration guide

--- 3-integration/connect-db/postgres/src/main/java/cafe/web/view/Cafe.java
+++ 3-integration/connect-db/postgres/src/main/java/cafe/web/view/Cafe.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
@@ -28,7 +28,7 @@ import javax.ws.rs.core.MediaType;
 import cafe.model.entity.Coffee;
 
 @Named
-@SessionScoped
+@RequestScoped
 public class Cafe implements Serializable {
 
 	private static final long serialVersionUID = 1L;
