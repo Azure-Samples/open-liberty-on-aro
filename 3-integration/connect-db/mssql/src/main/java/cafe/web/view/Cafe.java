436fab39679e1a4adff517e0fa0e806746e40900
Jianguo Ma <jiangma@microsoft.com>
2025-04-30 17:15:04 +0800

    cleanup for code sample 3-integration/connect-db/mssql
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- 3-integration/connect-db/mssql/src/main/java/cafe/web/view/Cafe.java
+++ 3-integration/connect-db/mssql/src/main/java/cafe/web/view/Cafe.java
@@ -15,74 +15,63 @@ import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.List;
 
 @Named
 @RequestScoped
-public class Cafe implements Serializable {
+public class Cafe {
 
-	private static final long serialVersionUID = 1L;
+    private String baseUri;
+    private Client client;
 
-	private String baseUri;
-	private transient Client client;
+    @NotNull
+    @NotEmpty
+    private String name;
+    @NotNull
+    private Double price;
 
-	@NotNull
-	@NotEmpty
-	protected String name;
-	@NotNull
-	protected Double price;
-	protected List<Coffee> coffeeList;
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Double getPrice() {
+        return price;
+    }
 
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public List<Coffee> getCoffeeList() {
-		this.getAllCoffees();
-		return coffeeList;
-	}
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
     public String getHostName() {
         return System.getenv("HOSTNAME");
     }
 
-	@PostConstruct
-	private void init() {
-		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+    @PostConstruct
+    private void init() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
             .getRequest();
-		baseUri = "http://localhost:9080" + request.getContextPath() + "/rest/coffees";
-		this.client = ClientBuilder.newBuilder().build();
-	}
+        baseUri = "http://localhost:9080" + request.getContextPath() + "/rest/coffees";
+        client = ClientBuilder.newBuilder().build();
+    }
 
-	private void getAllCoffees() {
-		this.coffeeList = this.client.target(this.baseUri).path("/").request(MediaType.APPLICATION_JSON)
+    public List<Coffee> getCoffeeList() {
+        return client.target(baseUri).path("/").request(MediaType.APPLICATION_JSON)
             .get(new GenericType<List<Coffee>>() {
-				});
-	}
+            });
+    }
 
-	public void addCoffee() throws IOException {
-		Coffee coffee = new Coffee(this.name, this.price);
-		this.client.target(baseUri).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
-		this.name = null;
-		this.price = null;
-		FacesContext.getCurrentInstance().getExternalContext().redirect("");
-	}
+    public void addCoffee() throws IOException {
+        Coffee coffee = new Coffee(name, price);
+        client.target(baseUri).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
+        FacesContext.getCurrentInstance().getExternalContext().redirect("");
+    }
 
-	public void removeCoffee(String coffeeId) throws IOException {
-		this.client.target(baseUri).path(coffeeId).request().delete();
-		FacesContext.getCurrentInstance().getExternalContext().redirect("");
-	}
+    public void removeCoffee(String coffeeId) throws IOException {
+        client.target(baseUri).path(coffeeId).request().delete();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("");
+    }
 }
